667c3d174086ecd7ca367edbd6e1274eb696af7c
Jack Casey <jackdcasey@gmail.com>
2020-05-03 23:41:03 -0700

    Add background gradient

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
-    <Header />
-    <CitiesList />
-
+  <div class="wrap">
+    <div id="app">
+      <Header />
+      <CitiesList />
+    </div>
   </div>
 </template>
 
@@ -20,10 +21,21 @@ export default {
 </script>
 
 <style>
+html,
+body {
+    height: 100%;
+}
+
+.wrap {
+background: rgb(9,0,162);
+background: linear-gradient(90deg, rgba(12,0,214,0.4) 0%, rgba(0,212,255,0.4) 100%);
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 </style>
