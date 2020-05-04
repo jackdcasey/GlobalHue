c2503049523bf05051126a9eb17d9f38b25041a9
Jack Casey <jackdcasey@gmail.com>
2020-05-03 23:30:48 -0700

    Make header sticky

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <nav class="navbar navbar-dark bg-dark">
+    <div class="wrapper">
+        <nav class="navbar navbar-fixed-top navbar-dark bg-dark">
         <a class="navbar-brand">GlobalHue</a>
         <span class="navbar-text">See the color of the sky, from around the world!</span>
         <a class="github-button" href="https://github.com/jackdcasey/globalhue" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star jackdcasey/globalhue on GitHub">Star</a>        </nav>
@@ -18,7 +18,12 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+
+.wrapper {
+  position: sticky;
+  top: 0;
+}
 
 span:first-of-type {
     flex: 1;
